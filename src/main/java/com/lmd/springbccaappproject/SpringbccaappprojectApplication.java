e56e06e3898888de44b486eccd99cc8e40cab587
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-02-25 13:28:48 -0600

    starting a new Spring project

---
+++ src/main/java/com/lmd/springbccaappproject/SpringbccaappprojectApplication.java
@@ -0,0 +1,13 @@
+package com.lmd.springbccaappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbccaappprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbccaappprojectApplication.class, args);
+	}
+
+}
